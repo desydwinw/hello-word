f138312cdb13eb84f410337840617f62dbbd11c6
Desi Dwi Nawang wulan <desydwi69@gmail.com>
2019-10-15 11:44:32 +0700

    Tambah Score Basket.

--- app/src/main/java/com/example/helloword/MainActivity.java
+++ app/src/main/java/com/example/helloword/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.helloword;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int jumlahA = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void tambah3poinA(View view) {
+
+        jumlahA = jumlahA+3;
+        TextView textviewA = findViewById(R.id.scoreA);
+        textviewA.setText(String.valueOf(jumlahA));
+
     }
 }
